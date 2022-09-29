9c65d47ee94fccf96f3e91ae4077775170315fbd
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-30 03:01:43 +0500

    add script to update and delete tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,84 @@
-import "./style.css";
-import "./modules/main.js";
+import './style.css';
+
+const itemList = document.querySelector('#myUL');
+const tasksArray = JSON.parse(localStorage.getItem('todo')) || [];
+
+class Todo {
+  constructor(description) {
+    this.id = tasksArray.length + 1;
+    this.description = description;
+    this.completed = false;
+  }
+
+  static updateIndex = () => {
+    tasksArray.forEach((data, index) => {
+      data.id = index + 1;
+    });
+  };
+
+  static removeTask = (index) => {
+    tasksArray.splice(index - 1, 1);
+    this.updateIndex();
+  };
+}
+
+const storage = (todo) => {
+  todo.sort((a, b) => a.index - b.index);
+  localStorage.setItem('todo', JSON.stringify(todo));
+};
+
+const populateHtml = () => {
+  storage(tasksArray);
+  itemList.innerHTML = tasksArray
+    .map(
+      (data) => `<li class='items'>
+          <div>
+            <input type='checkbox' ${
+              data.completed ? 'checked' : ''
+            } class='todo-item' name='car'>
+            <input for='' class='task' value='${data.description}'>
+          </div>
+          <div>
+          <button class='remove-btn'>x</button>
+          </div>
+        </li>`
+    )
+    .join(' ');
+  const removeBtn = document.querySelectorAll('.remove-btn');
+  removeBtn.forEach((btn, index) =>
+    btn.addEventListener('click', () => {
+      const item = index + 1;
+      Todo.removeTask(item);
+      populateHtml();
+    })
+  );
+};
+
+populateHtml();
+const toDoInput = document.querySelector('#todo-input');
+toDoInput.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    const description = toDoInput.value;
+    const newToDo = new Todo(description);
+    tasksArray.push(newToDo);
+    populateHtml();
+    toDoInput.value = '';
+  }
+});
+
+const label = document.querySelectorAll('.task');
+label.forEach((input, index) =>
+  input.addEventListener('change', () => {
+    tasksArray[index].description = input.value;
+    storage(tasksArray);
+  })
+);
+
+label.forEach((input, index) =>
+  input.addEventListener('click', () => {
+    const removeBtn = document.querySelectorAll('.remove-btn');
+    const option = document.querySelectorAll('.option');
+    removeBtn[index].style.display = 'inline';
+  })
+);
